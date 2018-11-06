4efde1762d5ac7ebd2639eba3a2ee3a4fda4837b
Moises Parra <m.parrita@hotmail.com>
2018-11-06 10:56:02 -0500

    usuario_confirmado

---
+++ usuario_confirmado.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Confirmación de usuario</title>
+</head>
+<body>
+    <h1>¡Su cuenta ha sido creada!</h1>
+    <h2>Bienvenidos a HelpMankind, <?php $nombre ?></h2>
+</body>
+</html>
